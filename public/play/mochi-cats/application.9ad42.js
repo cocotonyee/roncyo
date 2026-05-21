8b5f822cc40eaac5288f0c77e233d24b9978b7fb
Alex Chen <cocotonyee>
2026-05-21 21:17:07 +0800

    sa

--- public/play/mochi-cats/application.9ad42.js
+++ public/play/mochi-cats/application.9ad42.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.2acff.json';
+          this.settingsPath = 'src/settings.fd897.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
