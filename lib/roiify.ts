adce156142eabb2211bf7387f57b5d596d7aa083
Alex Chen <cocotonyee>
2026-06-09 18:41:55 +0800

    asdads

---
+++ lib/roiify.ts
@@ -0,0 +1,4 @@
+/** Roiify banner placement from publisher console */
+export const ROIIFY_BANNER_PLACEMENT_ID = "plc_3b7064bbsmtb";
+
+export const ROIIFY_SDK_URL = "https://www.roiify.net/sdk/roiify-ads.js";
